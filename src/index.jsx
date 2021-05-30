b8d1607e2cb71d22af12b028089b19c820150adb
Eva Janoskova <janoskova.evca@gmail.com>
2021-05-30 14:17:04 +0200

    component Joke added

--- src/index.jsx
+++ src/index.jsx
@@ -1,60 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 import { jokes } from './jokes';
+import Joke from './Joke';
 import './style.css';
 
-const App = () => {
-  const [like, setLike] = useState(0);
-  const [dislike, setDislike] = useState(0);
-
-  const handleLike = () => {
-    setLike(like + 1);
-  };
-
-  const handleDislike = () => {
-    setDislike(dislike + 1);
-  };
-  return (
-    <>
-      <div className="container">
-        <div className="joke">
-          <div className="joke__body">
-            <div className="joke__user">
-              <img
-                className="user-avatar"
-                src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-              />
-              <p className="user-name">Neroxx</p>
-            </div>
-
-            <p className="joke__text">
-              The secret service isn't allowed to yell "Get down!" anymore when
-              the president is about to be attacked. Now they have to yell
-              "Donald, duck!"
-            </p>
-          </div>
-          <div className="joke__likes">
-            <button
-              id="btn-up"
-              className="btn-like btn-like--up"
-              onClick={handleLike}
-            ></button>
-            <span id="likes-up" className="likes-count likes-count--up">
-              {like}
-            </span>
-            <button
-              id="btn-down"
-              className="btn-like btn-like--down"
-              onClick={handleDislike}
-            ></button>
-            <span id="likes-down" className="likes-count likes-count--down">
-              {dislike}
-            </span>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <div className="container">
+      {jokes.map((item) => (
+        <Joke
+          key={item.id}
+          userAvatar={item.avatar}
+          userName={item.name}
+          text={item.text}
+          likes={item.likes}
+          dislikes={item.dislikes}
+        />
+      ))}
+    </div>
+  </>
+);
 
 render(<App />, document.querySelector('#app'));
